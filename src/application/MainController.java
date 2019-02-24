37b2021af23aa567b151b6ebb50005f21e24b70d
Sacheth Swaminathan <>
2019-02-23 19:22:35 -0600

    Set Up all inputs for calculation

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -33,7 +33,7 @@ public class MainController implements Initializable {
     private TextField tf_annual_inflation;
 
     @FXML
-    private ChoiceBox<?> cb_type_of_fee;
+    private ChoiceBox<String> cb_type_of_fee;
 
     @FXML
     private TextField tf_annual_fee;
@@ -47,16 +47,23 @@ public class MainController implements Initializable {
      */
     @FXML
     void generateResultsClicked(ActionEvent event) {
-    	values = new TreeMap<TextField,Double>();
+    	
     	//Go through each node, check if a Text Field 
     	//Get input and store into a map
     	int count = 0;
     	for(TextField t : textFields) {
     		if(isGoodInput(t)) {
+    			
     			count++;
     		}
     	}
-    	
+    	if(count == 5) {
+    		for(TextField t : textFields) {
+    			double value = Double.parseDouble(t.getText().trim());
+    			values.put(t,value);
+    		}
+    		calculateReturn();
+    	}
     }
     /**
      * Initializes the nodes in the GUI
@@ -66,28 +73,24 @@ public class MainController implements Initializable {
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
+		values = new TreeMap<TextField,Double>();
 		textFields = new ArrayList<TextField>(5);
 		textFields.add(tf_initial_amount);
 		textFields.add(tf_annual_return);
 		textFields.add(tf_tax_rate);
 		textFields.add(tf_annual_inflation);
 		textFields.add(tf_annual_fee);
+		cb_type_of_fee.getItems().add("As a percentage of the interest earned");
+		cb_type_of_fee.getItems().add("As a percentage of the invested amount");
+		cb_type_of_fee.setValue("As a percentage of the interest earned");
 		
 	}
+
 	/**
-	 * Detects when a text field has been changed, and then changes the color to black
-	 * @param textField
+	 * Checks if the TextField has a number or not
+	 * @param t
+	 * @return a bool value based on if the input is correct or not
 	 */
-//	@FXML
-//	public void textTyped(ActionEvent event) {
-//		for(TextField t : textFields) {
-//			if(!isGoodInput(t)) {
-//				t.setStyle("-fx-text-inner-color: black");
-//				t.setText("");
-//			}
-//		}
-//	}
-	
 	public boolean isGoodInput(TextField t) {
 		double input;
 		try {
@@ -100,8 +103,15 @@ public class MainController implements Initializable {
 		}
 		return true;
 	}
-    
-    
+    /**
+     * Calculates the return on investment and then handles it
+     */
+    public void calculateReturn() {
+    	for(TextField t: textFields) {
+    		System.out.println(t.getText().trim());
+    	}
+    	
+    }
     
 
 }
