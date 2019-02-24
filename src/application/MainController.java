9a092e74baa23c20c4701f3b6216ebd5ca3f9867
Sacheth Swaminathan <>
2019-02-24 01:02:36 -0600

    GOT THE GRAPHS TO WORK !

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -35,7 +35,7 @@ public class MainController implements Initializable {
 	private ArrayList<Double> values; 
 	private ArrayList<TextField> textFields;
 	private double[] graph_values;
-	private double[] graph_values_inflation;
+	private double[] graph_values_real;
 
     @FXML
     private TextField tf_initial_amount;
@@ -65,7 +65,7 @@ public class MainController implements Initializable {
     private BarChart<String, Double> real_bar_graph;
 
     @FXML
-    private BarChart<String, Number> inflation_bar_graph;
+    private BarChart<?, ?> inflation_bar_graph;
     
     @FXML
     private CategoryAxis inflation_x_axis;
@@ -80,7 +80,7 @@ public class MainController implements Initializable {
     @FXML
     private NumberAxis real_y_axis;
 
-    XYChart.Series series1;
+    
     
     /**
      * Generates the return on investments, and does error checking
@@ -124,19 +124,19 @@ public class MainController implements Initializable {
 		generate_button.setOnAction(e -> generateResultsClicked());
 		reset_button.setOnAction(e -> resetValues());
 		
-//		inflation_x_axis = new CategoryAxis();
-//		inflation_y_axis = new NumberAxis();
-//        inflation_x_axis.setLabel("Years");       
-//        inflation_y_axis.setLabel("Amount of money");
-//        inflation_bar_graph.setTitle("Inflation adjusted ROI");
-//		inflation_bar_graph = new BarChart<String,Number>(inflation_x_axis,inflation_y_axis);
-//		series1 = new XYChart.Series();
-//		for(int i = 0;i<10;i++) {
-//			series1.getData().add(new XYChart.Data("Year " + (i+1), 100));
-//		}
-//		inflation_bar_graph.getData().add(series1);
-//		inflation_bar_graph.animatedProperty();
 		
+		inflation_x_axis = new CategoryAxis();
+		inflation_y_axis = new NumberAxis();
+        inflation_x_axis.setLabel("Years");       
+        inflation_y_axis.setLabel("Amount of money");
+        inflation_bar_graph.setTitle("Inflation adjusted ROI");
+        
+        real_x_axis = new CategoryAxis();
+		real_y_axis = new NumberAxis();
+        real_x_axis.setLabel("Years");       
+        real_y_axis.setLabel("Amount of money");
+        real_bar_graph.setTitle("Real adjusted ROI");
+	}
 		
 	/**
 	 * Resets the values of the TextFields, and resets the graphs
@@ -173,32 +173,53 @@ public class MainController implements Initializable {
     	System.out.println("Calculating Return on investment for " + years + " years");
     	//Values for calculation
     	double initial_amount = values.get(0);
+    	double real_initial_amount = values.get(0);
     	double annual_return = values.get(1) / 100.0;
     	double tax_rate = values.get(2) / 100.0;
     	double inflation_rate = values.get(3) / 100.0;
     	double annual_fee = values.get(4) / 100.0;
     	boolean percentage_of_intrest = (cb_type_of_fee.getValue() == "As a percentage of the interest earned");
     	graph_values = new double[years];
-    	graph_values_inflation = new double[years];
+    	graph_values_real = new double[years];
+
+    	//Creating data for graphs
+    	XYChart.Series series1 = new XYChart.Series();
+    	XYChart.Series series2 = new XYChart.Series();
+    	inflation_bar_graph.getData().clear();
+    	real_bar_graph.getData().clear();
     	
-    	//Calculating and then increasing Initial Amount
-    	series1.getData().clear();
+    	//Calculating and then increasing Initial Amount , inflated
     	for(int i = 0;i < years;i++) {
     		double return_val = (initial_amount * annual_return);
+    		double real_return_val = 0;
     		return_val *=(1 - tax_rate);
     		return_val *= (1 + inflation_rate);
+    		real_return_val = return_val / (1 + inflation_rate);
     		return_val -= (percentage_of_intrest)?(return_val * annual_fee):(initial_amount * annual_fee);
+    		real_return_val -= (percentage_of_intrest)?(return_val * annual_fee):(initial_amount * annual_fee);
     		System.out.println("Return for year " + (i + 1) + " " + initial_amount);
     		initial_amount += return_val;
     		graph_values[i] = initial_amount;
-//    		series1.getData().add(new XYChart.Data("Year " + (i+1), initial_amount));
-//    		XYChart.Data<Number, Number> data = 
-//                    (XYChart.Data<Number, Number>)series1.getData().get(i);
-//            data.setYValue(initial_amount);
+    		series1.getData().add(new XYChart.Data<String, Number>("" + (i+1), initial_amount));
+    		
     	}
+    	
+    	//For real adjustment
+    	for(int i = 0;i < years;i++) {
+    		double real_return_val = (real_initial_amount * annual_return);
+    		real_return_val *=(1 - tax_rate);
+    		real_return_val -= (percentage_of_intrest)?(real_return_val * annual_fee):(real_initial_amount * annual_fee);
+    		real_return_val -= (percentage_of_intrest)?(real_return_val * annual_fee):(real_initial_amount * annual_fee);
+    		System.out.println("Return for year " + (i + 1) + " " + real_initial_amount);
+    		real_initial_amount += real_return_val;
+    		graph_values_real[i] = real_initial_amount;
+    		series2.getData().add(new XYChart.Data<String, Number>("" + (i+1), real_initial_amount));
+    		
+    	}
+    	
     	//Creating the graphs
-    	//series1.getData().clear();
-    	//inflation_bar_graph.getData().addAll(series1);
+    	inflation_bar_graph.getData().addAll(series1);
+    	real_bar_graph.getData().addAll(series2);
         
     }
     
